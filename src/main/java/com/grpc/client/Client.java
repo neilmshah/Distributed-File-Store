7dd633194e74f52c5baac0d00c4e7c5cec8b9335
sricheta92 <srichetaruj.iste@gmail.com>
2018-11-28 15:07:10 -0800

    compilatio fix

--- src/main/java/com/grpc/client/Client.java
+++ src/main/java/com/grpc/client/Client.java
@@ -17,27 +17,7 @@ public class Client {
 
 	private static void loadConfig() {
 		ConfigUtil c = new ConfigUtil();
-		final ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080")
-		        .usePlaintext(true)
-		        .build();
-
-		      // It is up to the client to determine whether to block the call
-		      // Here we create a blocking stub, but an async stub,
-		      // or an async stub with Future are always possible.
-		      GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
-		      GreetingServiceOuterClass.HelloRequest request =
-		        GreetingServiceOuterClass.HelloRequest.newBuilder()
-		          .setName("Ray")
-		          .build();
-
-		      // Finally, make the call using the stub
-		      GreetingServiceOuterClass.HelloResponse response = 
-		        stub.greeting(request);
-
-		      System.out.println(response);
-
-		      // A Channel should be shutdown before stopping the process.
-		      channel.shutdownNow();
+		
 
 		
 	}
