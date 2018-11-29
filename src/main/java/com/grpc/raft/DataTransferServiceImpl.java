4c3600b05ba8eefce511002e1c4019d4103142c4
Vishnu Narayana <vishtheshnu@gmail.com>
2018-11-28 21:40:09 -0800

    began working on raft stuff

--- src/main/java/com/grpc/raft/DataTransferServiceImpl.java
+++ src/main/java/com/grpc/raft/DataTransferServiceImpl.java
@@ -2,13 +2,20 @@ package com.grpc.raft;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 import grpc.DataTransferServiceGrpc;
 import grpc.FileTransfer;
 import io.grpc.stub.StreamObserver;
 
 public class DataTransferServiceImpl extends DataTransferServiceGrpc.DataTransferServiceImplBase{
-	
+
+	private ConcurrentHashMap data;
+
+	public DataTransferServiceImpl(ConcurrentHashMap map){
+		data = map;
+	}
+
 	public void  RequestFileUpload(FileTransfer.FileUploadInfo request, StreamObserver<FileTransfer.ProxyList> responseObserver) {
 		
 		List<FileTransfer.ProxyInfo> activeProxies = new ArrayList<FileTransfer.ProxyInfo>();
--- src/main/java/com/grpc/raft/RaftServer.java
+++ src/main/java/com/grpc/raft/RaftServer.java
@@ -1,22 +1,30 @@
 package com.grpc.raft;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 /**
- * Server participating in RAFT
+ * Node in a raft cluster. Contains all of the stored data and methods
+ * related to the internal workings of the node itself. See Impl classes
+ * for networking implementations.
  */
-public class RaftServer 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Raft!" );
-        Server server = ServerBuilder.forPort(8080)
-          .addService(new DataTransferServiceImpl())
-          .build();
+public class RaftServer {
 
+	private ConcurrentHashMap data;
+	private int raftState; //0 for follower, 1 for candidate, 2 for leader
+	private int term;
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello Raft!" );
+        RaftServer myRaftServer = new RaftServer();
+        Server server = ServerBuilder.forPort(8080)
+          .addService(new DataTransferServiceImpl(myRaftServer.data))
+          .build();
 
         try {
 			server.start();
@@ -24,6 +32,9 @@ public class RaftServer
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		//Begin timeout
+
 
         System.out.println("Raft Server started");
         
@@ -33,5 +44,15 @@ public class RaftServer
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.println("Awaiting server termination");
     }
+
+    /*
+    Called when a leader times out
+     */
+	public void leaderTimeout(){
+    	//Become candidate, send out vote
+		raftState = 1;
+		
+	}
 }
