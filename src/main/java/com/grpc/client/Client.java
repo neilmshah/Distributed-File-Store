d77433c43c03b27e4301397d98fd523c6b4195ba
sricheta92 <srichetaruj.iste@gmail.com>
2018-11-29 08:41:25 -0800

    Removed test frfom client

--- src/main/java/com/grpc/client/Client.java
+++ src/main/java/com/grpc/client/Client.java
@@ -15,37 +15,7 @@ public class Client {
 
 	public static void main(String[] args) {
 
-		loadConfig();
-		final ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:3000")
-				.usePlaintext(true)
-				.build();
-
-		DataTransferServiceGrpc.DataTransferServiceStub stub = DataTransferServiceGrpc.newStub(channel);
-		StreamObserver<FileTransfer.FileUploadData> uploadFile = stub.uploadFile(new StreamObserver<FileTransfer.FileInfo>() {
-			@Override
-			public void onNext(FileTransfer.FileInfo value) {
-				System.out.println("file: " + value.getFileName());
-			}
-
-			@Override
-			public void onError(Throwable t) {
-
-			}
-
-			@Override
-			public void onCompleted() {
-
-			}
-		});
-
-
-	}
-
-	private static void loadConfig() {
-		ConfigUtil c = new ConfigUtil();
-
-
-
+	
 	}
 
 }
