7686e2fdd4fd207073f436f9f531cf435def6e69
Anav Sharma <anav.sharma@sjsu.edu>
2018-11-30 16:43:11 -0800

    fix for command line args

--- src/main/java/com/grpc/db/Datastore.java
+++ src/main/java/com/grpc/db/Datastore.java
@@ -44,7 +44,7 @@ public class Datastore {
         server.awaitTermination();
     }
 
-    public static void main(){
+    public static void main(String[] args){
         Datastore myDs = new Datastore();
         myDs.start();
         try {
